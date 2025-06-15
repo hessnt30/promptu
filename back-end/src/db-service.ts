f557e8b6950eda1b6ac8cbd2394634017e7be3a2
ashirazi01 <adrian01@vt.edu>
2025-06-15 12:55:06 -0400

    Adds sql queries for interacting with db

--- back-end/src/db-service.ts
+++ back-end/src/db-service.ts
@@ -1,9 +1,11 @@
 import {
   enablePromise,
   openDatabase,
+  ResultSet,
+  SQLErrors,
   SQLiteDatabase
 } from 'react-native-sqlite-storage';
-import { Prompt } from 'models/prompt';
+import { Prompt } from 'models';
 
 enablePromise(true);
 
@@ -11,10 +13,30 @@ export const getDBConnection = async () => {
   return openDatabase({ name: 'initial-prompts.db', location: 'default' });
 };
 
-export const createTable = async (tableName: string, db: SQLiteDatabase) => {
-  const query = `CREATE TABLE IF NOT EXISTS ${tableName}(
-        value TEXT NOT NULL
-    );`;
-
+export const createPromptTable = async (db: SQLiteDatabase) => {
+  const query = `CREATE TABLE IF NOT EXISTS prompts(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT NOT NULL, category TEXT NOT NULL);`;
   await db.executeSql(query);
+};
+
+export const getPrompts = async (
+  category: string,
+  db: SQLiteDatabase
+): Promise<Prompt[]> => {
+  try {
+    const prompts: Prompt[] = [];
+    const results = await db.executeSql(
+      `SELECT text FROM prompts WHERE prompts.category = ?;`,
+      [category]
+    );
+    results.forEach((result) => {
+      for (let i = 0; i < result.rows.length; i++) {
+        const prompt = result.rows.item(i) as Prompt;
+        prompts.push(prompt);
+      }
+    });
+    return prompts;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to retrieve prompts');
+  }
 };
